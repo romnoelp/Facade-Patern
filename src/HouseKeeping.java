cfe9c7b734842d7fb770d4eb50286b8d3ec28f3c
romnoelp <petracorta.rmnl@gmail.com>
2024-09-29 13:08:22 +0800

    Finished the HouseKeeping class.

--- src/HouseKeeping.java
+++ src/HouseKeeping.java
@@ -1,3 +1,16 @@
-public class HouseKeeping {
-    
+public class HouseKeeping implements HotelService {
+    private int roomNumber;
+
+    public HouseKeeping(int roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    @Override
+    public void service() {
+        cleanRoom();
+    }
+
+    public void cleanRoom() {
+        System.out.println("Cleaning room number " + roomNumber);
+    }
 }
