afe338eeb3c4d03fddfe7639ca468f1efb3d0a97
romnoelp <petracorta.rmnl@gmail.com>
2024-09-29 13:11:46 +0800

    Finished FrontDesk class

--- src/FrontDesk.java
+++ src/FrontDesk.java
@@ -1,3 +1,16 @@
 public class FrontDesk {
-    
+    public void requestValet(String plateNumber) {
+        HotelService valet = new Valet(plateNumber);
+        valet.service();
+    }
+
+    public void requestHouseKeeping(int roomNumber) {
+        HotelService houseKeeping = new HouseKeeping(roomNumber);
+        houseKeeping.service();
+    }
+
+    public void requestCart(int numberOfCarts) {
+        HotelService cart = new Cart(numberOfCarts);
+        cart.service();
+    }
 }
