efe0062a2eddd72aab07f6733fc01851598344b9
romnoelp <petracorta.rmnl@gmail.com>
2024-09-29 13:04:04 +0800

    Finished Valet class

--- src/Valet.java
+++ src/Valet.java
@@ -1,3 +1,16 @@
-public class Valet {
-    
+public class Valet implements HotelService{
+    private final String plateNumber;
+
+    public Valet(String plateNumber) {
+        this.plateNumber = plateNumber;
+    }
+
+    @Override
+    public void service() {
+        pickUpVehicle();
+    }
+
+    public void pickUpVehicle() {
+        System.out.println("Picking up vehicle with plate number " + plateNumber);
+    }
 }
