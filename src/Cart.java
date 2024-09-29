e0539df8fbb81554e0f5d3fdd8d62e2061b275d0
romnoelp <petracorta.rmnl@gmail.com>
2024-09-29 13:10:35 +0800

    Cart class finished.

--- src/Cart.java
+++ src/Cart.java
@@ -1,3 +1,16 @@
-public class Cart {
-    
+public class Cart implements HotelService {
+    private final int numberOfCarts;
+
+    public Cart(int numberOfCarts) {
+        this.numberOfCarts = numberOfCarts;
+    }
+
+    @Override
+    public void service() {
+        requestCart();
+    }
+
+    public void requestCart() {
+        System.out.println("Requesting " + numberOfCarts + " carts.");
+    }
 }
